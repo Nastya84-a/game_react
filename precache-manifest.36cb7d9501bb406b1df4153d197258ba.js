c0919346d386caa70fa9439ca819333ef7411f3f
Anastasya Kushnir <anastasyakyshnir@gmail.com>
2020-11-09 15:45:08 +0200

    Updates

---
+++ precache-manifest.36cb7d9501bb406b1df4153d197258ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7d1b7f601a9ecf42f45a1c7156e510e",
+    "url": "/game_react/index.html"
+  },
+  {
+    "revision": "0aac8d2015a687f9e30b",
+    "url": "/game_react/static/css/main.43704aa0.chunk.css"
+  },
+  {
+    "revision": "a76345f9d1aa9e52b0df",
+    "url": "/game_react/static/js/2.57737e5c.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/game_react/static/js/2.57737e5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0aac8d2015a687f9e30b",
+    "url": "/game_react/static/js/main.27614514.chunk.js"
+  },
+  {
+    "revision": "c059e639d9722946014c",
+    "url": "/game_react/static/js/runtime-main.9b21f868.js"
+  }
+]);
